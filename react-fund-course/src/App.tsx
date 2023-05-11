be8c10938bf9320a15a53d70e5ab1147b94f4095
yarski25 <yaroslav.saprykin@gmail.com>
2023-05-11 17:14:15 +0200

    functional counter implementation

--- react-fund-course/src/App.tsx
+++ react-fund-course/src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Counter from './components/Counter';
 
 function App() {
+
+  const [value, setValue] = useState('TEXT IN INPUT')
+
   return (
     <div className="App">
+      <Counter />
     </div>
   );
 }
