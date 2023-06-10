a4a981835714e83448f336d6401820ac12fca3a3
yarski25 <yaroslav.saprykin@gmail.com>
2023-06-10 23:04:44 +0200

    authorization logic improved

--- react-fund-course/src/api/auth.ts
+++ react-fund-course/src/api/auth.ts
@@ -2,7 +2,7 @@ import { BaseQueryFn, createApi } from '@reduxjs/toolkit/query/react';
 import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 import { AuthResponse } from '../types/interfaces/response/AuthResponse';
 
-export const API_URL = `http://localhost:4000/api`;
+export const API_URL = process.env.REACT_APP_API_URL as string;
 
 // export interface User {
 //   first_name: string;
@@ -48,10 +48,38 @@ const axiosBaseQuery =
   async ({ url, method, data, params }) => {
     try {
       axios.defaults.withCredentials = true;
-      const accessToken = localStorage.getItem('access_token');
-      if (accessToken) {
-        axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-      }
+      //   const accessToken = localStorage.getItem('access_token');
+      //   if (accessToken) {
+      //     axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+      //   }
+
+      axios.interceptors.request.use((config) => {
+        config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+        return config;
+      });
+
+      axios.interceptors.response.use(
+        (config) => {
+          return config;
+        },
+        async (error) => {
+          const originalRequest = error.config;
+          if (error.response.status == 401 && error.config && !error.config._isRetry) {
+            originalRequest._isRetry = true;
+            try {
+              const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {
+                withCredentials: true,
+              });
+              localStorage.setItem('token', response.data.accessToken);
+              return axios.request(originalRequest);
+            } catch (e) {
+              console.log('not authorized');
+            }
+          }
+          throw error;
+        },
+      );
+
       const result = await axios({ url: baseUrl + url, method, data, params });
       return { data: result.data };
     } catch (axiosError) {
@@ -72,12 +100,24 @@ export const authAPI = createApi({
   }),
   tagTypes: ['Auth'],
   endpoints: (builder) => ({
+    registration: builder.mutation<AuthResponse, LoginRequest>({
+      query: (credentials) => ({
+        url: '/registration',
+        method: 'POST',
+        data: credentials,
+      }),
+    }),
     login: builder.mutation<AuthResponse, LoginRequest>({
       query: (credentials) => ({
         url: '/login',
         method: 'POST',
         data: credentials,
-        //withCredentials: true,
+      }),
+    }),
+    logout: builder.mutation<AuthResponse, null>({
+      query: () => ({
+        url: '/logout',
+        method: 'POST',
       }),
     }),
   }),
